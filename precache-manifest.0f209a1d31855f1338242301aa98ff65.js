6c536f0c0adbbf46771ce8b4c710d255a8f6906d
Ekaterina Kurochka <ekaterina1.kur@gmail.com>
2019-07-01 08:45:47 +0300

    Updates

---
+++ precache-manifest.0f209a1d31855f1338242301aa98ff65.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d090b1d6f1e882c462cc3af69a629468",
+    "url": "/List_of_todos_React/index.html"
+  },
+  {
+    "revision": "e33d5ab56d746a23989b",
+    "url": "/List_of_todos_React/static/css/main.552c3d31.chunk.css"
+  },
+  {
+    "revision": "571c3a24e3947367ffa3",
+    "url": "/List_of_todos_React/static/js/2.25e08047.chunk.js"
+  },
+  {
+    "revision": "e33d5ab56d746a23989b",
+    "url": "/List_of_todos_React/static/js/main.6a330a96.chunk.js"
+  },
+  {
+    "revision": "cb6800034b65022a7c67",
+    "url": "/List_of_todos_React/static/js/runtime~main.5f4e8543.js"
+  }
+]);
